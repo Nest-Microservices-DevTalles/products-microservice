36bb55d04c5cfc9089aec32a7d54b86b62edc9ee
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-28 13:29:53 -0500

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+
+
+@Module({
+  imports: [ProductsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
