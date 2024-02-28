8de0c566dfa524128a17c4737aa91251160fce44
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-28 14:39:03 -0500

    Refactor pagination logic in ProductsController and ProductsService

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -17,9 +17,7 @@ export class ProductsController {
   @Get()
   findAll( @Query() paginationDto: PaginationDto ) {
 
-    return paginationDto;
-
-    // return this.productsService.findAll();
+    return this.productsService.findAll( paginationDto );
   }
 
   @Get(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -21,8 +22,24 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     
   }
 
-  findAll() {
-    return this.product.findMany({});
+  async findAll( paginationDto: PaginationDto ) {
+
+    const { page, limit } = paginationDto;
+
+    const totalPages = await this.product.count();
+    const lastPage = Math.ceil( totalPages / limit );
+
+    return {
+      data: await this.product.findMany({
+        skip: ( page - 1 ) * limit,
+        take: limit
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastPage: lastPage,
+      }
+    }
   }
 
   findOne(id: number) {
