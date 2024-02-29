2d4f3fd27b1d490ec86db20c6fc8450e203a0529
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-29 11:40:48 -0500

    Update error handling in ProductsService

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
+
 import { PaginationDto } from 'src/common';
 
 @Injectable()
@@ -51,7 +53,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if ( !product ) {
-      throw new NotFoundException(`Product with id #${ id } not found`);
+      throw new RpcException({ 
+        message: `Product with id #${ id } not found`,
+        status: HttpStatus.BAD_REQUEST
+      });
     }
 
     return product;
