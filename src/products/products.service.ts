56ee97d7ac206bcebbe23d395c747bd90eb57df6
Fernando Herrera <fernando.herrera85@gmail.com>
2024-03-02 11:12:26 -0500

    Add product validation endpoint

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -8,7 +8,6 @@ import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
-
   private readonly logger = new Logger('ProductsService');
 
   onModuleInit() {
@@ -17,71 +16,62 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   create(createProductDto: CreateProductDto) {
-    
     return this.product.create({
-      data: createProductDto
+      data: createProductDto,
     });
-    
   }
 
-  async findAll( paginationDto: PaginationDto ) {
-
+  async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
 
     const totalPages = await this.product.count({ where: { available: true } });
-    const lastPage = Math.ceil( totalPages / limit );
+    const lastPage = Math.ceil(totalPages / limit);
 
     return {
       data: await this.product.findMany({
-        skip: ( page - 1 ) * limit,
+        skip: (page - 1) * limit,
         take: limit,
         where: {
-          available: true
-        }
+          available: true,
+        },
       }),
       meta: {
         total: totalPages,
         page: page,
         lastPage: lastPage,
-      }
-    }
+      },
+    };
   }
 
   async findOne(id: number) {
-    const product =  await this.product.findFirst({
-      where:{ id, available: true }
+    const product = await this.product.findFirst({
+      where: { id, available: true },
     });
 
-    if ( !product ) {
-      throw new RpcException({ 
-        message: `Product with id #${ id } not found`,
-        status: HttpStatus.BAD_REQUEST
+    if (!product) {
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
       });
     }
 
     return product;
-
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
-
     const { id: __, ...data } = updateProductDto;
 
-
     await this.findOne(id);
-    
+
     return this.product.update({
       where: { id },
       data: data,
     });
-
-
   }
 
   async remove(id: number) {
-
     await this.findOne(id);
-    
+
     // return this.product.delete({
     //   where: { id }
     // });
@@ -89,12 +79,33 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const product = await this.product.update({
       where: { id },
       data: {
-        available: false
-      }
+        available: false,
+      },
     });
 
     return product;
+  }
 
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    });
+
+    if ( products.length !== ids.length ) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+
+    return products;
 
   }
 }
